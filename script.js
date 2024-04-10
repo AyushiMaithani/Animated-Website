9a90446a2408b972390145f8634ad4f5f61773f3
Ayushi Maithani <ayushimaithani04@gmail.com>
2024-04-10 20:22:08 +0530

    cursor added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ gsap.to("#nav",{
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start: "top -5%",
         end:"top -8%",
         scrub:1
@@ -19,7 +19,16 @@ gsap.to("#main",{
         scroller:"body",
         start:"top -30%",
         end:"top -80%",
-        markers:true,
+        // markers:true,
         scrub:2,
     }
+})
+
+var crsr = document.querySelector("#cursor");
+var blur=document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left=dets.x+"px";
+    crsr.style.top=dets.y+"px";
+    blur.style.left=dets.x -100+"px";
+    blur.style.top=dets.y-100+"px";
 })
